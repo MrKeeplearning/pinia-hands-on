8373062cb6091a0de26c3368720476400f959dce
MrKeeplearning <fossilhunter@hanmail.net>
2024-04-10 23:19:12 +0900

    pinia store

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,26 +1,32 @@
 import { defineStore } from 'pinia'
 
 export const useAuthStore = defineStore('auth', {
-  state: () => ({
-    isAuthenticated: false,
-    user: {}
-  }),
+  state() {
+    return {
+      isAuthenticated: false,
+      user: {}
+    };
+  },
   getters: {
-    fullName: (state) => `${state.user.first_name} ${state.user.last_name}`
+    fullName(state) {
+      return `${state.user.first_name} ${state.user.last_name}`;
+    }
   },
   actions: {
     logout() {
-      this.$patch((state) => {
-        (state.isAuthenticated = false), (state.user = {})
-      })
+      this.$patch(state => {
+        state.isAuthenticated = false;
+        state.user = {};
+      });
     },
     async login() {
-      // 정보를 불러오는데 delay를 부여할 수도 있다. 아래는 2초의 delay를 부여
-      const res = await fetch('https://reqres.in/api/users/2?delay=2')
-      const { data } = await res.json()
-      console.log(data)
-      this.user = data
-      this.isAuthenticated = true
+      const res = await fetch('https://reqres.in/api/users/2');
+      const { data } = await res.json();
+      console.log(data.data);
+      this.$patch(state => {
+        state.user = data;
+        state.isAuthenticated = true;
+      });
     }
   }
-})
+});
--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,29 +1,27 @@
 import { defineStore } from 'pinia'
-import { useAuthStore } from './auth.js';
+import { useAuthStore } from './auth.js'
 
 export const useCounterStore = defineStore('counter', {
-  // state의 대부분은 스토어를 중시으로 이루어진다.
-  // 일반적으로 앱을 나타내는 상태를 정의하는 것으로 시작.
-  // 피니아에서 state는 초기 상태를 반환하는 함수로 정의된다.
-  state: () => ({
-    count: 0
-  }),
-  getters: {
-    countDigitLength: (state) => state.count.toString().length
+  state() {
+    return { count: 0 };
   },
-  actions: {
-    increment() {
-      if (!this.isAuthenticated()) return;
-      this.count++
-    },
-    decrement() {
-      if (!this.isAuthenticated()) return;
-      this.count--
+  getters: {
+    countDigitLength(state) {
+      return state.count.toString().length;
     },
     isAuthenticated() {
-      // 로그인이 안되어 있으면 증가 기능을 실행하지 않는다.
       const authStore = useAuthStore();
       return authStore.isAuthenticated;
     }
+  },
+  actions: {
+    increment() {
+      if (!this.isAuthenticated) return;
+      this.count++;
+    },
+    decrement() {
+      if (!this.isAuthenticated) return;
+      this.count--;
+    }
   }
-})
+});
